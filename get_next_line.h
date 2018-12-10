8b424aef647e0fbca75371c15c7e670e9bc3ad3e
Remi DA CONCEICAO <reda-con@e1r9p11.42.fr>
2018-12-10 12:35:25 +0100

    get_next_line: the header

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: reda-con <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/19 08:44:13 by reda-con          #+#    #+#             */
+/*   Updated: 2018/12/04 17:56:39 by reda-con         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFF_SIZE 1
+# include "libft/inc/libft.h"
+
+int		get_next_line(int fd, char **line);
+
+#endif
